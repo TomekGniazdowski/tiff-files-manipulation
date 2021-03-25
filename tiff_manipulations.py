fb2c3ec307a3578455c51c727b3244acf7bd973d
MichalLopatka <62525358+MichalLopatka@users.noreply.github.com>
2021-03-25 19:36:17 +0100

    Update tiff_manipulations.py

--- tiff_manipulations.py
+++ tiff_manipulations.py
@@ -17,7 +17,7 @@ class Tiff_manipulations:
         self.offset_1 = self.return_1_offset()
         self.number_of_dic_entr = int(self.return_sum(self.data_hex_list[self.offset_1 : self.offset_1 + 2]), 16)
         self.main_header = self.offset_1 + 2
-        self.hexrange = len(self.data_hex_list) - 12
+        self.hexrange = self.main_header + 12 * self.number_of_dic_entr
 
 
     def check_required_fields(self):
@@ -29,10 +29,14 @@ class Tiff_manipulations:
     def return_1_offset(self):
         header = int('0x0004', 16)
         sum_of_elem = int(self.return_sum(self.data_hex_list[header: header + 4]), 16)
+
         return sum_of_elem
 
     def check_header(self, header):
         return int(self.return_sum(self.data_hex_list[header: header + 2]), 16)
+
+    def check_number_of_values(self, header):
+        return int(self.return_sum(self.data_hex_list[header + 4: header + 8]), 16)
 
     def list_data(self):
         dictionary = vars(self)
@@ -105,15 +109,42 @@ class Tiff_manipulations:
 
     def proper_type_place(self, header):
         chunk_type = self.check_type(header)
-        if chunk_type == 0x0001 or chunk_type == 0x0002:
-            return int(self.return_sum(self.data_hex_list[header + 8: header + 9]), 16)
-        if chunk_type == 0x0003:
-            return int(self.return_sum(self.data_hex_list[header + 8: header + 10]), 16)
-        if chunk_type == 0x0004:
-            return int(self.return_sum(self.data_hex_list[header + 8: header + 12]), 16)
-        if chunk_type == 0x0005:
-            header_copy = int(self.return_sum(self.data_hex_list[header + 8: header + 12]), 16)
-            return int(self.return_sum(self.data_hex_list[header_copy: header_copy + 4]), 16), \
-                   int(self.return_sum(self.data_hex_list[header_copy + 4: header_copy + 8]), 16)
+        number_of_values = self.check_number_of_values(header)
+        if number_of_values == 1:
+            if chunk_type == 0x0001 or chunk_type == 0x0002:
+                return int(self.return_sum(self.data_hex_list[header + 8: header + 9]), 16)
+            if chunk_type == 0x0003:
+                return int(self.return_sum(self.data_hex_list[header + 8: header + 10]), 16)
+            if chunk_type == 0x0004:
+                return int(self.return_sum(self.data_hex_list[header + 8: header + 12]), 16)
+            if chunk_type == 0x0005:
+                header_copy = int(self.return_sum(self.data_hex_list[header + 8: header + 12]), 16)
+                return int(self.return_sum(self.data_hex_list[header_copy: header_copy + 4]), 16), \
+                       int(self.return_sum(self.data_hex_list[header_copy + 4: header_copy + 8]), 16)
+            else:
+                return -1
+
         else:
-            return -1
+            data_array = []
+            if chunk_type == 0x0001 or chunk_type == 0x0002:
+                header_copy = int(self.return_sum(self.data_hex_list[header + 8: header + 9]), 16)
+                for i in range(number_of_values-1):
+                    data_array.append(int(self.return_sum(self.data_hex_list[header_copy+i: header_copy+i+1]), 16))
+
+                return data_array
+            if chunk_type == 0x0003:
+                number_of_values = 2 * number_of_values
+                header_copy = int(self.return_sum(self.data_hex_list[header + 8: header + 10]), 16)
+                for i in range(0, number_of_values-2, 2):
+                    data_array.append(int(self.return_sum(self.data_hex_list[header_copy+i: header_copy+i+2]), 16))
+                return data_array
+
+            if chunk_type == 0x0004:
+                number_of_values = 4 * number_of_values
+                header_copy = int(self.return_sum(self.data_hex_list[header + 8: header + 12]), 16)
+                for i in range(0, number_of_values-4, 4):
+                    data_array.append(int(self.return_sum(self.data_hex_list[header_copy+i: header_copy+i+4]), 16))
+                return data_array
+
+            else:
+                return -1
